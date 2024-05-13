07d2e113eeac339b43377f8917328051c5a15302
TPalma-2023 <palmaalv@msu.edu>
2024-05-13 08:45:18 -0400

    Utilize NODE_ENV to direct to production or development environment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,12 @@ export const APIKey = "82998fd41dea3b7833c39f5b9d310b64";
 export const latitude = 38.88;
 export const longitude = -77.0;
 
-export const baseUrl = "http://localhost:3001";
+// export const baseUrl = "http://localhost:3001";
+
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://whattowear.twilightparadox.com/"
+    : "http://localhost:3001";
 
 export const weatherOptions = [
   {
